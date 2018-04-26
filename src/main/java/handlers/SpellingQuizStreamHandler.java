c45b2ed67a4801df9d39b17b27e88189a45f528f
rfarley <rfarley001@yahoo.com>
2018-04-25 21:11:59 -0500

    Change files to spelling quiz launch

---
+++ src/main/java/handlers/SpellingQuizStreamHandler.java
@@ -0,0 +1,19 @@
+package handlers;
+
+import com.amazon.ask.Skill;
+import com.amazon.ask.SkillStreamHandler;
+import com.amazon.ask.Skills;
+
+public class SpellingQuizStreamHandler extends SkillStreamHandler {
+
+    private static Skill getSkill() {
+        return Skills.standard()
+                .addRequestHandlers(new CancelandStopIntentHandler(), new HelpIntentHandler(), new LaunchRequestHandler(), new SessionEndedRequestHandler())
+                .build();
+    }
+
+    public SpellingQuizStreamHandler() {
+        super(getSkill());
+    }
+
+}
